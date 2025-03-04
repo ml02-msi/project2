af4f1897c4c9b656788116f66fb5003bbe5f270f
ml02-msi <shaonballavstarinco@gmail.com>
2025-03-04 19:28:56 +0530

    Adding app.py file

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, HTTPException, Query, Response
+from fastapi.middleware.cors import CORSMiddleware
+import os
+import requests
+import subprocess
+import json
+from dotenv import load_dotenv
+from datetime import datetime
+from dateutil import parser
+from typing import Dict, Any
+import base64
+import numpy as np
+import pandas as pd
+import sqlite3, duckdb, httpx
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
+
+load_dotenv() 
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+AIPROXY_TOKEN = os.getenv("AIPROXY_TOKEN")
